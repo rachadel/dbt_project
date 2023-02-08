0bcffa88a302304e72e00ca3ab77d98d0330d904
Daniel Rachadel <daniracha@gmail.com>
2023-02-08 19:01:55 +0000

    v1.0

---
+++ models/employees.sql
@@ -0,0 +1,13 @@
+with
+    calc_employees as (
+        select
+            first_name || ' ' || last_name as name,
+            date_part(year, current_date) - date_part(year, birth_date) as age,
+            date_part(year, current_date)
+            - date_part(year, hire_date) as lenghtofservice,
+            *
+        from {{ source("sources", "employees") }}
+    )
+
+select *
+from calc_employees
